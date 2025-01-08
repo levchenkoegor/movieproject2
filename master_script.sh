07e30f06e157c11c13333552d314dd12dc5ca65a
elevchenko <levchenkoegors@gmail.com>
2025-01-08 17:21:57 +0000

    tonotopy script

--- master_script.sh
+++ master_script.sh
@@ -6,3 +6,4 @@ sudo nohup bash analysis/s1_convert_to_bids.sh > analysis/logs/s1_convert_to_bid
 nohup bash analysis/s2_freesurfer.sh > analysis/logs/s2_freesurfer.out &
 nohup bash analysis/s3_qc_moviewatching_afni.sh > analysis/logs/s3_qc_moviewatching_afni.out &
 nohup bash analysis/s4_qc_somatotopy_afni.sh > analysis/logs/s4_qc_somatotopy_afni.out &
+nohup bash analysis/s5_qc_tonotopy_afni.sh > analysis/logs/s5_qc_tonotopy_afni.out &
