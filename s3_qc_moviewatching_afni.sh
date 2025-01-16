59b0a1105e51016c964ba9052a43d7745eee7bc9
elevchenko <levchenkoegors@gmail.com>
2025-01-16 19:01:51 +0000

    subjects array

--- s3_qc_moviewatching_afni.sh
+++ s3_qc_moviewatching_afni.sh
@@ -10,9 +10,7 @@ max_jobs=8
 subjects="01 02 03 04 05 06 07 08 09 10 11 12 13 14 16 17 18 19 20 21 22 23 24 25 26 27 29 30 31 32 33 34 35 36 37 38 39 40 42 43 44"
 
 # Run AFNI
-# Done: 01 02 03 04 05 06 10 24 36
-
-for subject_id in 07 08 09 10 11 12 13 14; do #$subjects; do
+for subject_id in $subjects; do
 
     (
       # Start timing
