45d24e7fe7575acf34c19889e05bff60db32ffbe
elevchenko <levchenkoegors@gmail.com>
2025-01-16 19:01:40 +0000

    subjects array

--- s2_freesurfer.sh
+++ s2_freesurfer.sh
@@ -20,7 +20,7 @@ fi
 
 
 # Run
-for subj_id in 16 17 18 19 20 21 22 23 24 25 26 27 29 30 31 32 33 34 35 36 37 38 39 40 42 43 44; do #$subjects; do
+for subj_id in $subjects; do
 
     if [[ $subj_id -eq "10" ]]; then
         # Use a different path for subject 10
