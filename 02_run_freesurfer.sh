157fe7650ce83136f102a81fddcc4026397ce0cd
elevchenko <levchenkoegors@gmail.com>
2025-02-03 15:27:16 +0000

    assign subjects variable dynamically

--- 02_run_freesurfer.sh
+++ 02_run_freesurfer.sh
@@ -8,7 +8,8 @@ source $FREESURFER_HOME/SetUpFreeSurfer.sh
 export SUBJECTS_DIR=/data/elevchenko/MovieProject2/bids_data/derivatives/freesurfer/
 export data_folder=/data/elevchenko/MovieProject2/bids_data
 
-subjects="01 02 03 04 05 06 07 08 09 10 11 12 13 14 16 17 18 19 20 21 22 23 24 25 26 27 29 30 31 32 33 34 35 36 37 38 39 40 42 43 44"
+# Extract subject IDs dynamically from the bids_data folder
+subjects=$(ls -d $data_folder/sub-* | awk -F'/' '{print $NF}' | sed 's/sub-//')
 
 # Check if folder SUBJECTS_DIR exists
 if [ -d "$SUBJECTS_DIR" ]; then
