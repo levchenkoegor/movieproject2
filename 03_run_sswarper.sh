81f5d7444630fc8d96e3fc273a1c3971513c4ab7
elevchenko <levchenkoegors@gmail.com>
2025-02-03 15:27:52 +0000

    assign subjects variable dynamically

--- 03_run_sswarper.sh
+++ 03_run_sswarper.sh
@@ -4,9 +4,8 @@
 # Setup directories
 export data_folder=/data/elevchenko/MovieProject2/bids_data
 
-#subjects="01 02 03 04 05 06 07 08 09 10 11 12 13 14 16 17 18 19 20 21 22 23 24 25 26 27 29 30 31 32 33 34 35 36 37 38 39 40 42 43 44"
-
-subjects="01 02"
+# Extract subject IDs dynamically from the bids_data folder
+subjects=$(ls -d $data_folder/sub-* | awk -F'/' '{print $NF}' | sed 's/sub-//')
 
 # Run
 for subj_id in $subjects; do
@@ -27,6 +26,6 @@ for subj_id in $subjects; do
         -base MNI152_2009_template_SSW.nii.gz
 
     # Compress results
-    gzip "${data_folder}"/derivatives/"${subj_id}"/SSwarper/*nii
+    gzip "${data_folder}"/derivatives/sub-"${subj_id}"/SSwarper/*nii
 
 done
